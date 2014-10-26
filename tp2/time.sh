3a695049792aa28fbc5cc2beef5009eed49e38ae
marssol <marssolina@gmail.com>
2014-10-26 20:29:09 +0100

    makefile report

--- tp2/time.sh
+++ tp2/time.sh
@@ -5,12 +5,12 @@ TIME1=$(date +"%s%N")
 TIME2=$(date +"%s%N")
 TIME3=$(($TIME2-$TIME1))
 TIME3=$(($TIME3/1000000))
-echo "Execution time in sequential mode : "$TIME3" microseconds"
+echo "Execution time : "$TIME3" microseconds"
 
 TIME1=$(date +"%s%N")
 ./test1_bis /usr/bin/firefox outfile
 TIME2=$(date +"%s%N")
 TIME3=$(($TIME2-$TIME1))
 TIME3=$(($TIME3/1000000))
-echo "Execution time in multi-thread (2 threads) mode : "$TIME3" microseconds"
+echo "Execution time : "$TIME3" microseconds"
 
