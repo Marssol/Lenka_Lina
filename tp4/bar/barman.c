fb7d68236c7dd7a5001e18bb860631547f313322
LenkaKunikova <lenka.kun@gmail.com>
2014-11-18 16:15:00 +0100

    debugging

--- tp4/bar/barman.c
+++ tp4/bar/barman.c
@@ -47,8 +47,8 @@ static void print()
 	printf("\n* Barmans *\n");
 	for (i = 0; i < nb_barman; i++) {
 		switch(barman_states[i][0]) {
-			case THINKING : 
-				printf("T ");
+            case WAITING :
+                printf("W ");
 				break;
 			case BUSY : 
 				printf("B ");
@@ -87,25 +87,26 @@ void init(int n_clients, int m_barmans, int k_bottles, int **states_barmans, int
 	cv_clients = (pthread_cond_t*)(malloc(n_clients*sizeof(pthread_cond_t)));
 	cv_barmans = (pthread_cond_t*)(malloc(m_barmans*sizeof(pthread_cond_t)));
 	cv_bottles = (pthread_cond_t*)(malloc(k_bottles*sizeof(pthread_cond_t)));
+
+    client_choices = (Choice_bottles*)(malloc(n_clients*sizeof(Choice_bottles)));
 	
 	//Initialisation pthread_cond
 	pthread_cond_init (&cv_all_barmans, NULL);
 
 	int i;
+    for (i = 0; i < m_barmans; i++) {
+        pthread_cond_init (&cv_barmans[i], NULL);
+        barman_states[i][0] = DISPONIBLE;
+        barman_states[i][1] = -1;
+    }
 	for (i = 0; i < n_clients; i++) {
 		pthread_cond_init (&cv_clients[i], NULL);
 		clients_states[i] = THINKING;
-	}
-	for (i = 0; i < m_barmans; i++) {
-		pthread_cond_init (&cv_barmans[i], NULL);
-		barman_states[i][0] = DISPONIBLE;
-		barman_states[i][1] = -1;
-	}
+    }
 	for (i = 0; i < k_bottles; i++) {
-		pthread_cond_init (&cv_bottles[i], NULL);
-		barman_states[i][0] = AVAILABLE;
-	}
-	
+        pthread_cond_init (&cv_bottles[i], NULL);
+        bottle_states[i] = AVAILABLE;
+    }
 	nb_client = n_clients;
 	nb_barman = m_barmans;
 	nb_bottles = k_bottles;
@@ -123,15 +124,16 @@ void take_barman(int id_client, Choice_bottles str_set_bottles)
    
 	//Check if one barman is free
 	int i = 0;
-    while (barman_states[i][0] != DISPONIBLE) {
-		if (i == nb_barman) {
+    while (barman_states[i][0] != DISPONIBLE && i < nb_barman ){
+        if (i == nb_barman - 1) {
 			pthread_cond_wait (&cv_all_barmans,&mb);
+            i = 0;
 		}
 		i++;
 	}
-	
-	barman_states[i][0] = WAITING;
-	barman_states[i][1] = id_client;
+
+    barman_states[i][0] = WAITING;
+    barman_states[i][1] = id_client;
 	clients_states[nb_client] = SERVED;
 	
 	//The client choice is saved
@@ -144,7 +146,7 @@ void take_barman(int id_client, Choice_bottles str_set_bottles)
 	
 	//Show states tables
 	
-	printf("Client %d ask his drinking choice\n",id_client);
+    printf("Client %d drink\n",id_client);
 	print();
 	clients_states[nb_client] = DRINKING;
 	pthread_mutex_unlock (&mb);
--- tp4/bar/main.c
+++ tp4/bar/main.c
@@ -10,6 +10,8 @@
 #define NB_DRINKS 5 
 #define MAXIMUM_BOTTLES 3
 
+int bar_go_close = 0;
+
 //Argument structure
 typedef struct Array_arg {
 	int my_id;
@@ -27,11 +29,12 @@ int comp (const void * elem1, const void * elem2)
 
 Choice_bottles random_set_bottles (int k_bottles)
 {
-	int nb_bottles_drink = rand() % MAXIMUM_BOTTLES;
+    int nb_bottles_drink = rand() % MAXIMUM_BOTTLES +1;
 	int start = rand() % k_bottles;
 	int j, i = start;
 	int sum = 0;
-	int array[k_bottles];
+    //int array[k_bottles];
+    int* array = (int*)malloc(k_bottles*sizeof(int));
 	for (j = 0; j < k_bottles; j++) {
 		array[j] = 0;
 	}
@@ -52,19 +55,27 @@ Choice_bottles random_set_bottles (int k_bottles)
 	
 	Choice_bottles struct_set_bottles;
 	struct_set_bottles.nb_bottles = nb_bottles_drink;
-	struct_set_bottles.set_bottles = (int *) malloc(nb_bottles_drink * sizeof(int));
+    struct_set_bottles.set_bottles = (int *) malloc(nb_bottles_drink * sizeof(int));
+    if (struct_set_bottles.set_bottles<=0){
+       printf("error malloc");
+            exit (1);
+    }
+    printf("\n\n BLUP \n%d\n",struct_set_bottles.set_bottles);
 	sum = 0;
-	i = 0;
+    i = 0;
+    printf("random choise is: \n");
 	while (sum != nb_bottles_drink) { 
 		if (array[i] == 1) {
 			struct_set_bottles.set_bottles[sum] = i;
+            printf("%d ",i);
 			sum++;
 		}
-		i++;
+        i++;
 	}
-	
+    printf("\n");
 	qsort (struct_set_bottles.set_bottles, nb_bottles_drink, sizeof(int), comp);
 	
+    free(array);
 	return struct_set_bottles; 
 } 
 
@@ -98,7 +109,7 @@ void *f_client(void *arg)
 		sleep(DRINK_TIME);
 		
 		nb_boisson_drank++;
-		free_bottles(str_set_bot);
+        free_bottles(str_set_bot);
 	}
 	
 	return 0;
@@ -109,7 +120,7 @@ void * f_barman(void *arg)
 	Array_arg *argu = (Array_arg *)arg;
 	int id_barman = argu->my_id;
 	
-	while(!bar_close) {
+    while(!bar_go_close) {
 		
 		//Wait a client	
 		ready(id_barman);
@@ -129,7 +140,9 @@ void * f_barman(void *arg)
 
 int main (int argc, char **argv) 
 {
-	if (argc != 4) {
+     Choice_bottles cc = random_set_bottles (5);
+
+    if (argc != 4) {
 		fprintf(stderr, "usage : %s number of clients, barmans and botles\n", argv[0]);
 		exit (-1) ;
 	}
@@ -177,6 +190,16 @@ int main (int argc, char **argv)
 	pthread_t *pthreads_clients = (pthread_t *)malloc(nb_clients * sizeof(pthread_t));
 	pthread_t *pthreads_barmans = (pthread_t *)malloc(nb_barmans * sizeof(pthread_t));
 
+    for (i = 0; i < nb_barmans; i++) {
+        args_b[i].my_id = i;
+        args_b[i].k_bottles = nb_bottles;
+        if (pthread_create(&pthreads_barmans[i], NULL, f_barman, &args_b[i])) {
+
+                printf("ERROR !\n");
+                return -1;
+        }
+    }
+
 	for (i = 0; i < nb_clients; i++) {
 		args_c[i].my_id = i;
 		args_c[i].k_bottles = nb_bottles;
@@ -187,20 +210,13 @@ int main (int argc, char **argv)
         }
 	}
 	
-	for (i = 0; i < nb_barmans; i++) {
-		args_b[i].my_id = i;
-		args_b[i].k_bottles = nb_bottles;
-        if (pthread_create(&pthreads_barmans[i], NULL, f_barman, &args_b[i])) {
-
-				printf("ERROR !\n");
-				return -1;
-        }
-	}
-	
     //Wait all threads
     for (i = 0; i < nb_clients; i++) {
 		pthread_join(pthreads_clients[i], NULL);
 	}
+
+    bar_go_close = 1;
+
     for (i = 0; i < nb_barmans; i++) {
 		pthread_join(pthreads_barmans[i], NULL);
 	}
